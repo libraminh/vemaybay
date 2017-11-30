3d3c93abe4f6d0baae5a69788a111f9f72f8f7b6
libraminh <libraminh@gmail.com>
2017-11-30 16:46:37 +0700

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,4 +2,20 @@ new WOW().init();
 
 $(function(){
   // niceSelectFilter();
+
+  changePayIcon();
 })
+
+// Function Area
+function changePayIcon() {
+  var link = $('#pay-tab .card-header a');
+
+  if($('#pay-tab .collapse').is('.show')) {
+    $('.card-body .line').show();
+  }
+
+  $(link).on('click', function(){
+    $(this).find('i').toggleClass('fa-plus').toggleClass('fa-close');
+    // $(this).parents('.card').find('.line').css({'opacity' : '1'});
+  })
+}
